e0d9334817a02f257b4cc98ff8b3bcd49b1fef8f
Anthony Lorendeaux <anthony.lorendeaux19@gmail.com>
2022-02-07 22:24:00 +0100

    Create the task tracking project

---
+++ task-tracker/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <h1> {{title}} </h1>
+        <Button />
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+    export default {
+        name: 'Header',
+        props: {
+            title: {
+                type: String,
+                default: "Hello World"
+            }
+        },
+        components: {
+            Button
+        }
+    }    
+</script>
