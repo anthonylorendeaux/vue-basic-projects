52c2c6c09d6626e0f1d8999ccdb601ed1478801f
Anthony Lorendeaux <anthony.lorendeaux19@gmail.com>
2022-02-07 22:39:34 +0100

    Add basic button and format the template

--- task-tracker/src/App.vue
+++ task-tracker/src/App.vue
@@ -11,6 +11,33 @@ export default {
   name: 'App',
   components: {
     Header
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Test 1',
+        day: 'March 1st at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Test 2',
+        day: 'March 2st at 2:30pm',
+        reminder: false,
+      },
+      {
+        id: 3,
+        text: 'Test 3',
+        day: 'March 3st at 2:30pm',
+        reminder: false,
+      },
+    ]
   }
 }
 </script>
--- task-tracker/src/components/Button.vue
+++ task-tracker/src/components/Button.vue
@@ -1,9 +1,18 @@
 <template>
-    <button> Add Task </button>
+    <button v-on:click="onClick()"> {{text}} </button>
 </template>
 
 <script>
     export default {
-        name: 'Button'
+        name: 'Button',
+        props: {
+            text: String,
+            color: String
+        },
+        methods: {
+            onClick() {
+                console.log('Click')
+            }
+        }
     }
 </script>
