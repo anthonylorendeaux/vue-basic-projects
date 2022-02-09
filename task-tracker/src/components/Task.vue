d4fffd291b0756b17827196937be0f90d435f0f9
Anthony Lorendeaux <anthony.lorendeaux19@gmail.com>
2022-02-09 19:54:53 +0100

    Add basic data display

---
+++ task-tracker/src/components/Task.vue
@@ -0,0 +1,16 @@
+<template>
+    <h3> 
+        {{task.text}} 
+        <strong> X </strong>
+    </h3>
+    <p> {{task.day}} </p>
+</template>
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        task: Object
+    }
+}
+</script>
