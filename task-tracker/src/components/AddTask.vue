f2980c6fda90a4004822bd9ecbdae9fb284c375f
Anthony Lorendeaux <anthony.lorendeaux19@gmail.com>
2022-02-12 16:57:22 +0100

    add css to the form

--- task-tracker/src/components/AddTask.vue
+++ task-tracker/src/components/AddTask.vue
@@ -1,17 +1,20 @@
 <template>
-    <form @submit="onSubmit">
-        <label>Task</label>
-        <input type="text" v-model="text" name="text" placeholder="Add Task" />
-        <label>Day & Time</label>
+    <form @submit="onSubmit" class="mt-10 flex flex-col justify-center align-middle">
+        <label class="text-gray-700 text-lg font-semibold">Task</label>
+        <input class="border-2 border-gray-500 rounded-md p-2" type="text" v-model="text" name="text" placeholder="Add Task" />
+        <label class="mt-4 text-gray-700 text-lg font-semibold">Day & Time</label>
         <input
             type="text"
             v-model="day"
             name="day"
             placeholder="Add Day & Time"
+            class="border-2 border-gray-500 rounded-md p-2"
         />
-        <label>Set Reminder</label>
-        <input type="checkbox" v-model="reminder" name="reminder" />
-        <input type="submit" value="Save Task"/>
+        <div class="flex justify-between mt-4">
+            <label class="text-gray-700 text-md font-semibold">Set Reminder</label>
+            <input type="checkbox" v-model="reminder" name="reminder" />
+        </div>
+        <input class="rounded-xl p-1 mt-4 text-md font-semibold text-white bg-black hover:bg-gray-800" type="submit" value="Save Task"/>
     </form>
 </template>
 
